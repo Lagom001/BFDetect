ded3d094072b9a36855f3fa6a627d0a521723f9a
Lagom001 <edelmmitchell@gmail.com>
2021-08-13 00:47:52 -0400

    Changed time to epoch

--- BruteForce.sh
+++ BruteForce.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-echo "What file would you like to open?"
+echo 'What file would you like to open?'
 read in_pcap
-
-tshark -r "$in_pcap" -T fields -e ip.src -e tcp.dstport | sort | uniq -c | sort -k 1 -n -r  > "output.txt"
-#tshark -r "$in_pcap" -T fields -e ip.dst -e tcp.dstport | sort | uniq -c | sort -k 1 -n -r  > "output2.cvs"
+#Pull top src ip and dst port and calculate amount per ip/port
+tshark -r "$in_pcap" -T fields -e ip.src -e tcp.dstport | sort | uniq -c | sort -k 1 -n -r  > 'output.txt'
 
 head output.txt | awk '{print $2, $3}' > topips.txt
 awk '!$2{next}1' topips.txt > cleanlist.txt
 
-echo "Possible Brute Force Attackers: "
+echo 'Possible Brute Force Attackers: '
 cat cleanlist.txt
-
+#Sep  6, 2019 14:46:30.003887000 EDT
 while read -r key value;do
-    tshark -r "$in_pcap" -T fields -e frame.time 'ip.src== '$key' and tcp.dstport== '$value'' > $key.$value.txt
-    awk '{ total += $4 } END { print total/NR }' $key.$value.txt > mean.txt
-    cat mean.txt
-    #sort $key.$value.txt | awk '{print $2, $3}' > trimmed.txt | head -n1 - tail -n1
-    #Difference= $Headder - $Footer
-    #echo $Difference
+    tshark -r "$in_pcap" -T fields -e frame.time_epoch 'ip.src== '$key' and tcp.dstport== '$value'' | sort -n > $key.$value.txt
+    #date -d $key.$value.txt '+%b +%d, +%Y +%T.+%N +%Z' > mean.txt 
+    hd= head -n1 $key.$value.txt
+    tl= tail -n1 $key.$value.txt
+    echo $tl - $hd
     rm $key.$value.txt
-    rm mean.txt
 done < cleanlist.txt
 
 rm output.txt
